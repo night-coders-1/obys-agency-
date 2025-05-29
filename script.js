6ff308488e6fc5388bd1bde1912f75270e9a5092
Sourabh Singh <sourabhsingh8005@gmail.com>
2025-05-30 00:14:10 +0530

    code cleaned :)

--- script.js
+++ script.js
@@ -140,8 +140,14 @@ arrows.forEach((arrow) => {
   const circleText = arrow.querySelector(".circle-text");
 
   // Start hidden
-  gsap.set(whiteCircle, { scale: 0, opacity: 0 });
-  gsap.set(circleText, { scale: 0.9, opacity: 0 });
+  gsap.set(whiteCircle, {
+    scale: 0,
+    opacity: 0,
+  });
+  gsap.set(circleText, {
+    scale: 0.9,
+    opacity: 0,
+  });
 
   // On hover in
   arrow.addEventListener("mouseenter", () => {
@@ -291,51 +297,3 @@ links.forEach((link) => {
     });
   });
 });
-
-// todo hover
-
-
-// todo gooey animation
-// function gooeyAnimation() {
-//   Shery.imageEffect("#olga", {
-//     style: 5,
-//     config: {
-//       a: { value: 2, range: [0, 30] },
-//       b: { value: 0.75, range: [-1, 1] },
-//       zindex: { value: -9996999, range: [-9999999, 9999999] },
-//       aspect: { value: 0.8081953766802364 },
-//       ignoreShapeAspect: { value: true },
-//       shapePosition: { value: { x: 0, y: 0 } },
-//       shapeScale: { value: { x: 0.5, y: 0.5 } },
-//       shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-//       shapeRadius: { value: 0, range: [0, 2] },
-//       currentScroll: { value: 0 },
-//       scrollLerp: { value: 0.07 },
-//       gooey: { value: true },
-//       infiniteGooey: { value: false },
-//       growSize: { value: 4, range: [1, 15] },
-//       durationOut: { value: 1, range: [0.1, 5] },
-//       durationIn: { value: 1.5, range: [0.1, 5] },
-//       displaceAmount: { value: 0.5 },
-//       masker: { value: true },
-//       maskVal: { value: 1.31, range: [1, 5] },
-//       scrollType: { value: 0 },
-//       geoVertex: { range: [1, 64], value: 1 },
-//       noEffectGooey: { value: true },
-//       onMouse: { value: 1 },
-//       noise_speed: { value: 0.2, range: [0, 10] },
-//       metaball: { value: 0.34, range: [0, 2] },
-//       discard_threshold: { value: 0.5, range: [0, 1] },
-//       antialias_threshold: { value: 0, range: [0, 0.1] },
-//       noise_height: { value: 0.5, range: [0, 2] },
-//       noise_scale: { value: 10, range: [0, 100] },
-//     },
-//     gooey: true,
-
-//     // effect: "gooey",
-//     // intensity: 0.5,
-//     // duration: 1.5,
-//     ease: "power2.inOut",
-//   });
-// }
-// gooeyAnimation();
